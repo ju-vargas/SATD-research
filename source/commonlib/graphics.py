b282edcf6bc08160fe3faff48d2412b8a957c785
ju-vargas <ju.rodrigues0100@gmail.com>
2023-10-20 15:25:39 -0300

    graficos

--- source/commonlib/graphics.py
+++ source/commonlib/graphics.py
@@ -1,7 +1,7 @@
 from source.models import matrix as mtx
 
 
-def make_dictionary(matrix, config):
+def make_dictionary3(matrix, config):
 
     data = {
         'Class': ['A1', 'A1', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E'],
@@ -9,6 +9,36 @@ def make_dictionary(matrix, config):
         '1': [],
         '2': [],
         '3': [],
+        'sad': []
+    }
+
+    aproximations = matrix.get_aproximations()
+    for aprox in aproximations:   
+        if(aprox.get_type() != "Precise"):
+            data[aprox.get_type()].append(aprox.get_video("Campfire").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("FoodMarket4").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("BasketballDrive").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("RitualDance").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("BQMall").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("RaceHorses2").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("BasketballPass").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("RaceHorses").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("FourPeople").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("Johnny").get_BDRate(config))
+
+    return data
+
+    
+
+def make_dictionary4(matrix, config):
+
+    data = {
+        'Class': ['A1', 'A1', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E'],
+        'Name': ['Campfire', 'FoodMarket4', 'BasketballDrive', 'RitualDance', 'BQMall', 'RaceHorses2', 'BasketballPass', 'RaceHorses', 'FourPeople', 'Johnny'],
+        '1': [],
+        '2': [],
+        '3': [],
+        '4': [],
         'sad': []
     }
 
@@ -28,4 +58,28 @@ def make_dictionary(matrix, config):
 
     return data
 
-    
+def make_dictionary2(matrix, config):
+
+    data = {
+        'Class': ['A1', 'A1', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E'],
+        'Name': ['Campfire', 'FoodMarket4', 'BasketballDrive', 'RitualDance', 'BQMall', 'RaceHorses2', 'BasketballPass', 'RaceHorses', 'FourPeople', 'Johnny'],
+        '1': [],
+        '2': [],
+        'sad': []
+    }
+
+    aproximations = matrix.get_aproximations();
+    for aprox in aproximations:   
+        if(aprox.get_type() != "Precise"):
+            data[aprox.get_type()].append(aprox.get_video("Campfire").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("FoodMarket4").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("BasketballDrive").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("RitualDance").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("BQMall").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("RaceHorses2").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("BasketballPass").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("RaceHorses").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("FourPeople").get_BDRate(config))
+            data[aprox.get_type()].append(aprox.get_video("Johnny").get_BDRate(config))
+
+    return data
